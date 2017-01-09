d9359f2dc30b1e7eca4fab7998f8760a69a16756
Daniel Pineda Guerrero <danipine92@gmail.com>
2017-01-09 20:42:57 +0100

    Añadido un test de 3*2

--- src/test/TestMultiplicar.java
+++ src/test/TestMultiplicar.java
@@ -10,6 +10,11 @@ public class TestMultiplicar {
 		assertEquals(4, mult(2,2));
 	}
 	
+	@Test
+	public void testMultiplicar2(){
+		assertEquals(6, mult(3,2));
+	}
+	
 	int mult(int a, int b){
 		return multiplicar.Multiplicacion.multiplicacion(a, b);
 	}
